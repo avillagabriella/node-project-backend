a400134542e2b08830268db0d36fff55170d0253
avillagabriella <avillagabriella@outlook.com>
2024-01-04 14:28:47 -0300

    GET, POST and PATCH

--- mulheres.js
+++ mulheres.js
@@ -1,65 +1,101 @@
-const express = require("express") 
+const { request } = require("express")
+const express = require("express") // aqui estou iniciando o express
+const router = express.Router() // aqui estou configurando a primeira porta
+const { v4: uuidv4 } = require('uuid')
 
-const router = express.Router()
+const app = express() // aqui estou criando o app
+app.use(express.json())
+const porta = 3333 // aqui estou criando a pota
 
-const app = express()
-
-const porta = 3333
-
-
-
+// aqui estou criando a lista inicial de mulheres
 const mulheres = [
 
- {
+  {
 
-   nome: 'Simara Conceição',
+    id: '1',
+    nome: 'Simara Conceição',
+    imagem: 'https://bit.ly/3LJIyOF',
+    minibio: 'Desenvolvedora e instrutora',
 
-   imagem: 'https://bit.ly/3LJIyOF',
+  },
 
-   minibio: 'Desenvolvedora e instrutora',
+  {
+    id: '2',
+    nome: 'Iana Chan',
+    imagem: 'https://bit.ly/3JCXBqP',
+    minibio: 'CEO & Founder da PrograMaria',
 
- },
+  },
 
- {
+  {
+    id: '3',
+    nome: 'Luana Pimentel',
+    imagem: 'https://bit.ly/3FKpFaz',
+    minibio: 'Senior Staff Software Engineer',
 
-   nome: 'Iana Chan',
-
-   imagem: 'https://bit.ly/3JCXBqP',
-
-   minibio: 'CEO & Founder da PrograMaria',
-
- },
-
- {
-
-   nome: 'Luana Pimentel',
-
-   imagem: 'https://bit.ly/3FKpFaz',
-
-   minibio: 'Senior Staff Software Engineer',
-
- }
+  }
 
 ]
 
 
-
+// GET
 function mostraMulheres(request, response) {
 
-response.json(mulheres)
+  response.json(mulheres)
 
+}
+
+// POST
+function criaMulher(request, response) {
+  const novaMulher = {
+    id: uuidv4(),
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    minibio: request.body.minibio
+  }
+
+  mulheres.push(novaMulher)
+
+  response.json(mulheres)
 }
 
 
 
+// PATCH
+function corrigeMulher(request, response) {
+  function encontraMulher(mulher) {
+    if (mulher.id === request.params.id) {
+      return mulher;
+    }
+  }
+
+  const mulherEncontrada = mulheres.find(encontraMulher);
+
+  if (request.body.nome) {
+    mulherEncontrada.nome = request.body.nome;
+  }
+
+  if (request.body.minibio) {
+    mulherEncontrada.minibio = request.body.minibio;
+  }
+
+  if (request.body.imagem) {
+    mulherEncontrada.imagem = request.body.imagem;
+  }
+
+  response.json(mulheres);
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) // configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurei a rota PATCH /mulheres/:id
+
+
+// PORTA
 function mostraPorta() {
 
-    console.log("Servidor criado e rodando na porta ", porta)
+  console.log("Servidor criado e rodando na porta ", porta)
 
 }
 
-
-
-app.use(router.get('/mulheres', mostraMulheres))
-
-app.listen(porta, mostraPorta)
+app.listen(porta, mostraPorta) // servidor ouvindo a porta
